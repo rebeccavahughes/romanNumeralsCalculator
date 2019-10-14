2b2e873755c6f2b3e5a55be0dbc30b173c1aa3e9
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-10-14 12:07:44 +0100

    first commit

---
+++ romannumerals.js
@@ -0,0 +1,16 @@
+let result = '';
+const convertToRoman = (num) => {
+    
+    let decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    let roman = ["M", "CM","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"]
+
+    for(let i = 0;i<=decimal.length; i++){
+        while (num%decimal[i] < num){
+            result += roman[i];
+            num -= decimal[i];
+        }
+    }
+    return result;
+} 
+console.log(convertToRoman(8))
+
